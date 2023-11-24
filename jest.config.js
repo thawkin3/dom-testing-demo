37834e1f9f65727ac7e32f12c80803e884138688
thuyencode <thuyencode@proton.me>
2023-11-24 11:31:08 +0700

    Fix duplicated key in Jest config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   clearMocks: true,
-  setupFilesAfterEnv: ['regenerator-runtime/runtime'],
+  setupFilesAfterEnv: ['regenerator-runtime/runtime', './setupTests.js'],
   testPathIgnorePatterns: ['/node_modules/'],
-  setupFilesAfterEnv: ['./setupTests.js'],
 }
